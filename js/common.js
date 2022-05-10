04d9ccd4fc0343e3b93e24bc8cda97490f53aae8
kyubaek <brekqkim@naver.com>
2022-05-10 18:29:46 +0900

    달력 일정등록 리스트 완성 / 일정 리스트 닫기 기능 추가

--- js/common.js
+++ js/common.js
@@ -108,19 +108,58 @@ $(function() {
               }
               showCalendar();
 
-              // 다음달로 넘어갈 때. 현재 달 삭제됨.
-              function removeCalendar(){
-                  let catchTr = 100;
-                  for(var i = 100; i< 106; i++){
-                      var $tr = document.getElementById(catchTr);
-                      $tr.remove();
-                      catchTr++;
-                  }
-              }
+            // 다음달로 넘어갈 때. 현재 달 삭제됨.
+            function removeCalendar(){
+                let catchTr = 100;
+                for(var i = 100; i< 106; i++){
+                    var $tr = document.getElementById(catchTr);
+                    $tr.remove();
+                    catchTr++;
+                }
+            }
 
-              $("#cal .contents_wrap table tbody tr td").on("click", function() {
-                $("#cal .contents_wrap table tbody tr td").append('<div></div>');
-              });
+            // 날짜 클릭 및 각각의 창 나오기
+            function showMain(){
+            mainTodayDay.innerHTML = dayList[today.getDay()];
+            mainTodayDate.innerHTML = today.getDate();
+            }
+
+            // var clickedDate1 = document.getElementById(today.getDate());
+            // clickedDate1.classList.add('active');
+            // var prevBtn = document.getElementById('prev');
+            // var nextBtn = document.getElementById('next');
+            // prevBtn.addEventListener('click',prev);
+            // nextBtn.addEventListener('click',next);
+            
+            var tdGroup = [];
+            function clickStart(){
+                for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+                    tdGroup[i] = document.getElementById(i);
+                    tdGroup[i].addEventListener('click',changeToday);
+                }
+            }
+            function changeToday(e){
+                for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+                    if(tdGroup[i].classList.contains('active')){
+                        tdGroup[i].classList.remove('active');
+                    }
+                }
+                clickedDate1 = e.currentTarget;
+                clickedDate1.classList.add('active');
+                today = new Date(today.getFullYear(), today.getMonth(), clickedDate1.id);
+                showMain();
+                keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
+                reshowingList();
+            }
+
+            // todo_list 검은 화면 누르면 내려가기
+            $(".todo_list .bg").on("click",function(){
+                $(".todo_list .list").css("top","100%");
+                $(".todo_list .bg").fadeOut(500);
+                return false;
+            });
+
+
 
               break;
         
